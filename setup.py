b45f73e818014aa92107dee1629acbfa72dc51da
Lawrence Vriend <43742775+lcvriend@users.noreply.github.com>
2019-07-16 21:57:10 +0200

    Download shapefiles during setup

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from src.config import PARAM, PATH_RESOURCES
-from src.utils import get_dataset
+from src.utils import get_dataset, download_from_url
 
 get_dataset(PARAM.geonames, PATH_RESOURCES / 'geonames')
+
+for field, url in zip(PARAM.mapping._fields, PARAM.mapping):
+    download_from_url(url, path_out=PATH_RESOURCES / 'shapefiles' / field)
