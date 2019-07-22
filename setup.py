b8dd8befec2754dc9f66e9b1ee3592fab91b7fcf
Lawrence Vriend <43742775+lcvriend@users.noreply.github.com>
2019-07-22 12:56:38 +0200

    Save and unzip maps using fieldname

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+# standard library
+import zipfile
+from pathlib import Path
+
+# local
 from src.config import PARAM, PATH_RESOURCES
 from src.utils import get_dataset, download_from_url
 
 get_dataset(PARAM.geonames, PATH_RESOURCES / 'geonames')
 
 for field, url in zip(PARAM.mapping._fields, PARAM.mapping):
-    download_from_url(url, path_out=PATH_RESOURCES / 'shapefiles' / field)
+    suffix = Path(url).suffix
+    path_out = PATH_RESOURCES / 'shapefiles' / field
+    filename = f"{field}.{suffix}"
+
+    download_from_url(
+        url,
+        filename=filename,
+        path_out=path_out
+        )
+
+    if suffix == 'zip':
+        with zipfile.ZipFile(path_out / filename, 'r') as zip_ref:
+            zip_ref.extractall(path_out)
