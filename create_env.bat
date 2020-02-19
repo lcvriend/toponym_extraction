373d7662c634275acda63c34c6b1ec9203018db0
lcvriend <43742775+lcvriend@users.noreply.github.com>
2020-02-19 01:05:42 +0100

    Tweak batch file

--- create_env.bat
+++ create_env.bat
@@ -1,6 +1,6 @@
 call conda env create -f environment.yml
 call conda activate place-extraction
-call python -m ipykernel install --user --name place-extraction --display-name "place-extraction"
+call python -m ipykernel install --user --name place-extraction --display-name place-extraction
 call python -m spacy download en
 call python -m spacy download nl
 pause
